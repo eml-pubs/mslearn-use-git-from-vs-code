40c7ec93d45cbaa36f1d7ed9f16c138c6f79046c
Eric MAZUEL <eric.mazuel@asmodee.com>
2022-08-27 10:47:25 +0200

    updated response object

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello Git2' });
+  res.render('index', { title: 'Hello Git3' });
 });
 
 module.exports = router;
